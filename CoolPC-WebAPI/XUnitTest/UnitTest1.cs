7a492452b63aaae0886acd69ce28cc5c42a6bc65
Аксенов Александр Константинович <pr111aksenov@oat.local>
2024-04-23 13:33:23 +0600

    start creating test units

---
+++ CoolPC-WebAPI/XUnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using CoolPC_WebAPI;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        private readonly HttpClient _client;
+
+        //public AlbumApiTest()
+        //{
+        //    var server = new TestServer(new WebHostBuilder()
+        //        .UseEnviroment("Development")
+        //        .UserStartup<Startup>());
+        //    _client = server.CreateClient();
+        //}
+        //[Theory]
+        //[InlineData("GET")]
+        //public async Task AlbumGetAllTestAsyns(string method)
+        //{
+        //    var request = new HttpRequestMessage()
+        //}
+    }
+        
+}
+
