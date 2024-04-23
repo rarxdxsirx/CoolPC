829c5062f067dc767c9ce3c46304750568d9ef8d
Ковалева Валерия Андреевна <pr111kovaleva@oat.local>
2024-04-23 09:34:05 +0600

    first commit

---
+++ CoolPC-WebAPI/CoolPC-WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CoolPC_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CoolPC-WebAPI/CoolPC-WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoolPC_WebAPI.WebApiApplication" Language="C#" %>
